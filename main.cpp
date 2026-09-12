fec66392ae549190189b63d260608ec5be16be05
billf <bill.flink@gmail.com>
2026-09-12 11:33:34 -0500

    Add GPIO control for LED toggle functionality

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
+#include <gpiod.hpp>
+#include <exception>
 #include <iostream>
 
-int main() {
-    bool ledOn = false;
-    char command{};
-    std::cout << "Enter command (t = toggle led, q = quit): ";
-    
-     while(std::cin >> command) {
-        if(command == 't') {
-            ledOn = !ledOn;
-            std::cout << "LED is now " << (ledOn ? "ON" : "OFF") << std::endl;
-        } else if(command == 'q') {
-            break;
-        } else {
-            std::cout << "Unknown command" << std::endl;
+int main()
+{
+    try
+    {
+        constexpr unsigned int ledPin = 17;
+
+        gpiod::chip chip{"/dev/gpiochip0"};
+
+        auto request = chip.prepare_request()
+            .set_consumer("pi-panel")
+            .add_line_settings(
+                ledPin,
+                gpiod::line_settings{}
+                    .set_direction(gpiod::line::direction::OUTPUT)
+                    .set_output_value(gpiod::line::value::INACTIVE))
+            .do_request();
+
+        bool ledOn = false;
+        char command{};
+
+        std::cout << "t = toggle LED, q = quit\n";
+
+        while (std::cin >> command)
+        {
+            if (command == 'q')
+                break;
+
+            if (command == 't')
+            {
+                ledOn = !ledOn;
+
+                request.set_value(
+                    ledPin,
+                    ledOn ? gpiod::line::value::ACTIVE
+                          : gpiod::line::value::INACTIVE);
+
+                std::cout << "LED: " << (ledOn ? "ON" : "OFF") << '\n';
+            }
         }
-        std::cout << "Enter command (t = toggle led, q = quit): ";
+
+        request.set_value(ledPin, gpiod::line::value::INACTIVE);
     }
-    return 0;
+    catch (const std::exception& error)
+    {
+        std::cerr << "GPIO error: " << error.what() << '\n';
+        return 1;
+    }
 }
